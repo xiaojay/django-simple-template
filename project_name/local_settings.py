e8faf6305254a1eb341a6d3112fddcc52094894b
jay young <xiaojay@gmail.com>
2013-03-15 09:35:04 +0800

    initial

---
+++ project_name/local_settings.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+import os
+PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_DIR, 'db'),
+    }
+}
