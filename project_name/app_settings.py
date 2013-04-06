0a0c05802791734d715e166852640578a5af378b
jay young <xiaojay@gmail.com>
2013-04-06 15:38:47 +0800

    delete app settings for hcrab and add media url

--- project_name/app_settings.py
+++ project_name/app_settings.py
@@ -2,8 +2,3 @@
 #global setting for installed app
 import os
 PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
-
-#setting for app hcrab
-HOST = 'http://download.jaylab.org'
-VIDEO_DIR = ''
-EXPIRE_TIME = 300
